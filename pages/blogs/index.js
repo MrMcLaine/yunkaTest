b7d51f9a2da374226004f58e421862c33f69e476
MrMcLaine <Lypovskyi@gmail.com>
2023-06-19 16:04:52 +0300

    The structure was changed. Added new directories about, blogs, join

---
+++ pages/blogs/index.js
@@ -0,0 +1,8 @@
+
+const Index = () => {
+    return (
+        <h1>Blog page</h1>
+    )
+}
+
+export default Index;
---
+++ pages/join/index.js
@@ -0,0 +1,8 @@
+
+const Index = () => {
+    return (
+        <h1>Join page</h1>
+    )
+}
+
+export default Index;
